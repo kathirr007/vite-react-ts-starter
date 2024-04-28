a05f0b7277e91b8c24c7d3bdde3a83f90258328d
Kathiravan K <kathirr007@gmail.com>
2024-04-28 19:43:03 +0530

    updated editorconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,9 @@ export default function HomePage() {
     <div className="p-3">
       <h1 className="text-3xl text-center my-4">Home Page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem temporibus illum a nihil, tempore aliquid vero corrupti laborum ab quos! Velit eos voluptatem inventore corrupti, maxime consequuntur ea repellendus culpa?</p>
+      <div className="card">
+        <h3>Test Card 01</h3>
+      </div>
     </div>
   )
 }
